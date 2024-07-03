10711759cfe66a3f8c2d2af32810a810302d14a6
pratikshinde788 <>
2024-07-03 17:41:30 +0530

    New screens added

---
+++ src/main/java/com/mahait/gov/in/controller/DesignationMasterController.java
@@ -0,0 +1,17 @@
+package com.mahait.gov.in.controller;
+
+import java.util.Locale;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/mdc")
+@Controller
+public class DesignationMasterController {
+	@GetMapping("/designationMaster")
+	public String designationMaster(Model model, Locale locale, HttpSession session) {
+		return "/views/designation-master";
+	}
+}
