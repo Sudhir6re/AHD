dea9778e143010abe8f4b04a92cd4504db464998
pratikshinde788 <>
2024-07-10 17:36:26 +0530

    MDC screens added

---
+++ src/main/java/com/mahait/gov/in/controller/EmpDistWiseValidationReportController.java
@@ -0,0 +1,23 @@
+package com.mahait.gov.in.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/mdc")
+@Controller
+public class EmpDistWiseValidationReportController {
+
+	@GetMapping("/empDistWiseSearch")
+	public String empDistWiseSearch(Model model, Locale locale, HttpSession session) {
+		return "/views/emp-dist-wise-val-search";
+	}
+	@GetMapping("/empDistWiseValReport")
+	public String empDistWiseValReport(Model model, Locale locale, HttpSession session) {
+		return "/views/reports/emp-dist-wise-val-report";
+	}
+}
