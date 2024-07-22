c10e74a31cbc57bb0dad4f89e6f298e7719f946d
unknown <tejashree.egov@gmail.com>
2024-07-22 16:04:21 +0530

    committed for employee statistics

--- src/main/java/com/mahait/gov/in/controller/EmployeeStatisticsController.java
+++ src/main/java/com/mahait/gov/in/controller/EmployeeStatisticsController.java
@@ -39,7 +39,7 @@ import com.mahait.gov.in.model.TopicModel;
 import com.mahait.gov.in.service.CommonHomeMethodsService;
 import com.mahait.gov.in.service.EmployeeStatisticsService;
 @Controller
-@RequestMapping("/paybill")
+@RequestMapping("/ddoast")
 public class EmployeeStatisticsController {
 	
 //	protected final Log logger = LogFactory.getLog(getClass());
@@ -76,161 +76,10 @@ public class EmployeeStatisticsController {
 		model.addAttribute("language", locale.getLanguage());
 
 		SimpleDateFormat sdf =new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-		lstempstcBillObj.clear();
-				String empname=null;
-				String savaarthid=null;
-				String dob=null;
-				String empType=null;
-				String cadre=null;
-				String postname=null;
-				String postType=null;
-				String poststartdate=null;
-				String postenddate=null;
-				String dateofjoining=null;
-				String dateofexipry=null;
-				String scaledec=null;
-				String basicpay=null;
-				String sevenbasicpay=null;
-				String pfserives=null;
-				String gisgroup=null;
-				String gisvalue=null;
-				String gpfdcps=null;
-				String handicap=null;
-				String bankname=null;
-				String bankbarname=null;
-				String ifsccode=null;
-				String accno=null;
-				String pranno=null;
-				String pranstatus=null;
-				String pranremarks=null;
-				String aadharno=null;
-				String panno=null;
-		List<Object[]> listA = employeeStatisticsService.findEmployeeStatistics(messages.getDdoCode());
-		for (Object[] objLst : listA) {
-			if (objLst[0] != null) {
-				empname=objLst[0].toString();
-			}
-			if (objLst[1] != null) {
-				savaarthid=objLst[1].toString();
-			}else {
-				savaarthid="";	
-			}
-			if (objLst[2] != null) {
-				dob=objLst[2].toString();
-			}
-			if (objLst[3] != null) {
-				pranno=objLst[3].toString();
-			}
-			if (objLst[4] != null) {
-				pranstatus=objLst[4].toString();
-			}
-			if (objLst[5] != null) {
-				pranremarks=objLst[5].toString();
-			}
-			if (objLst[6] != null) {
-				if(objLst[6].toString()=="P") {
-					empType="Permanent";
-				}else if(objLst[6].toString()=="B") {
-					empType="Both";
-				}else {
-					empType="Temporary";
-				}
-				empType="Permanent";
-			}
-			if (objLst[7] != null) {
-				cadre=objLst[7].toString();
-			}
-			if (objLst[8] != null) {
-				postname=objLst[8].toString();
-			}
-		
-			if (objLst[9] != null) {
-				if(objLst[9].toString()=="P") {
-					postType="Permanent";
-				}else if(objLst[9].toString()=="B") {
-					postType="Both";
-				}else {
-					postType="Temporary";
-				}
-				postType="Permanent";
-				
-			}
-			if (objLst[10] != null) {
-				poststartdate=objLst[10].toString();
-			}
-			if (objLst[11] != null) {
-				postenddate=objLst[11].toString();
-			}
-			if (objLst[12] != null) {
-				dateofjoining=objLst[12].toString();
-			}
-			if (objLst[13] != null) {
-				dateofexipry=objLst[13].toString();
-			}
-			if (objLst[14] != null) {
-				scaledec=objLst[14].toString();
-			}
-			
-			if (objLst[15] != null) {
-				basicpay=objLst[15].toString();
-				
-			}
-			if (objLst[16] != null) {
-				sevenbasicpay=objLst[16].toString();
-			}else {
-				sevenbasicpay="";
-			}
-			if (objLst[17] != null) {
-				pfserives=objLst[17].toString();
-			}else {
-				pfserives="";
-			}
-			if (objLst[18] != null) {
-				gpfdcps=objLst[18].toString();
-			}
-			if (objLst[19] != null) {
-				gisgroup=objLst[19].toString();
-			}
-			if (objLst[20] != null) {
-				gisvalue=objLst[20].toString();
-			}
-			if (objLst[21] != null) {
-				handicap=objLst[21].toString();
-			}else {
-				handicap="-";
-			}
-			if (objLst[22] != null) {
-				bankname=(objLst[22].toString());
-			}
-			if (objLst[23] != null) {
-				bankbarname=(objLst[23].toString());
-			}
-			if (objLst[24] != null) {
-				ifsccode=objLst[24].toString();
-			}
-			if (objLst[25] != null) {
-				accno=objLst[25].toString();
-			}
-			if (objLst[26] != null) {
-				aadharno=objLst[26].toString();
-				}else {
-					aadharno="";
-			}
-			if (objLst[27] != null) {
-				panno=objLst[27].toString();
-			}else {
-				panno="";
-			}
-		
-			
-			employeeststicReportModel(empname,savaarthid,dob,empType,cadre,postname,postType,poststartdate,postenddate,dateofjoining,
-					 dateofexipry,scaledec,basicpay,sevenbasicpay,pfserives,gpfdcps,gisgroup,gisvalue,handicap,bankname,bankbarname,
-					 ifsccode,accno,pranno,pranstatus,pranremarks,aadharno,panno);
-		}
 		
 		model.addAttribute("createddate", sdf.format(new Date()));
 		model.addAttribute("systemdate", sdf.format(new Date()));
-		  model.addAttribute("lstempstcBillObj",lstempstcBillObj);
+		  model.addAttribute("empList",employeeStatisticsService.findEmployeeStatistics(messages.getDdoCode()));
 		  return "/views/reports/employeeStatistics";
 		
 	}
@@ -258,7 +107,7 @@ public class EmployeeStatisticsController {
 	XSSFWorkbook workbook = new XSSFWorkbook();
 	XSSFSheet sheet = workbook.createSheet("Data");
 	
-	List<Object[]> lstResult = employeeStatisticsService.findEmployeeStatistics(messages.getUserName());
+	List<EmployeeStatisticsModel> lstResult = employeeStatisticsService.findEmployeeStatistics(messages.getDdoCode());
 	
 	if(lstResult!=null && lstResult.size()>0){
 		 Row headerRow = sheet.createRow(0);
@@ -269,25 +118,25 @@ public class EmployeeStatisticsController {
 		 headerCell = headerRow.createCell(2);
 		 headerCell.setCellValue("Date of Birth");
 		 headerCell = headerRow.createCell(3);
-		 headerCell.setCellValue("Pran No");
-		 headerCell = headerRow.createCell(4);
-		 headerCell.setCellValue("Pran Status");
-		 headerCell = headerRow.createCell(5);
-		 headerCell.setCellValue("Pran Remarks");
-		 headerCell = headerRow.createCell(6);
 		 headerCell.setCellValue("Employee Type");
-		 headerCell = headerRow.createCell(7);
-		 headerCell.setCellValue("Cadre");
-		 headerCell = headerRow.createCell(8);
+		 headerCell = headerRow.createCell(4);
+		 headerCell.setCellValue("Cadrex");
+		 headerCell = headerRow.createCell(5);
 		 headerCell.setCellValue("Post Name");
-		 headerCell = headerRow.createCell(9);
+		 headerCell = headerRow.createCell(6);
 		 headerCell.setCellValue("Post Type");
-		 headerCell = headerRow.createCell(10);
+		 headerCell = headerRow.createCell(7);
 		 headerCell.setCellValue("Post Start Date");
-		 headerCell = headerRow.createCell(11);
-		 headerCell.setCellValue("Post End Date");
-		 headerCell = headerRow.createCell(12);
+		 headerCell = headerRow.createCell(8);
+		 headerCell.setCellValue("Post end Date");
+		 headerCell = headerRow.createCell(9);
 		 headerCell.setCellValue("Date of Joining");
+		 headerCell = headerRow.createCell(10);
+		 headerCell.setCellValue("Date of Service Expiry");
+		 headerCell = headerRow.createCell(11);
+		 headerCell.setCellValue("Scale Description");
+		 headerCell = headerRow.createCell(12);
+		 headerCell.setCellValue("Basic Salary");
 		 headerCell = headerRow.createCell(13);
 		 headerCell.setCellValue("Date of Service Expiry");
 		 headerCell = headerRow.createCell(14);
@@ -297,24 +146,26 @@ public class EmployeeStatisticsController {
 		 headerCell = headerRow.createCell(16);
 		 headerCell.setCellValue("Seven PC Basic");
 		 headerCell = headerRow.createCell(17);
-		 headerCell.setCellValue("PF Series");
+		 headerCell.setCellValue("7PC Level");
 		 headerCell = headerRow.createCell(18);
-		 headerCell.setCellValue("GPF/DCPS Account No");
+		 headerCell.setCellValue("PF Series");
 		 headerCell = headerRow.createCell(19);
-		 headerCell.setCellValue("GIS Group");
+		 headerCell.setCellValue("GPF/DCPS Account No");
 		 headerCell = headerRow.createCell(20);
-		 headerCell.setCellValue("GIS Value");
+		 headerCell.setCellValue("GIS Group");
 		 headerCell = headerRow.createCell(21);
-		 headerCell.setCellValue("Physically Handicapped");
+		 headerCell.setCellValue("GIS Value");
 		 headerCell = headerRow.createCell(22);
-		 headerCell.setCellValue("Bank Name");
+		 headerCell.setCellValue("Physically Handicapped");
 		 headerCell = headerRow.createCell(23);
-		 headerCell.setCellValue("Bank Branch Name");
+		 headerCell.setCellValue("Bank name");
 		 headerCell = headerRow.createCell(24);
-		 headerCell.setCellValue("IFSC Code");
+		 headerCell.setCellValue("Bank Branch Name");
 		 headerCell = headerRow.createCell(25);
 		 headerCell.setCellValue("Account No");
-//		 headerCell = headerRow.createCell(26);
+		 headerCell = headerRow.createCell(26);
+		 headerCell.setCellValue("Pran No");
+		/// headerCell = headerRow.createCell(26);
 		 int i=1;
 		 int  srNo=1;
 		 
@@ -323,9 +174,7 @@ public class EmployeeStatisticsController {
 			Object obj[] = (Object[]) iterator.next();
 			Row row = sheet.createRow(i++);
 			 
-//			Cell cell = row.createCell(rowNo++);
 			Cell cell;
-//			cell.setCellValue(srNo++);
 
 			 for (int j = 0; j < obj.length; j++) {//Read Column from index 1
 				 if(j==0){
@@ -334,7 +183,7 @@ public class EmployeeStatisticsController {
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==1){
-					 cell = row.createCell(rowNo++);//For Sevarth Id
+					 cell = row.createCell(rowNo++);//For sevaarthId
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
@@ -344,21 +193,26 @@ public class EmployeeStatisticsController {
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==3){
-					 cell = row.createCell(rowNo++);//For Pran No
+					 cell = row.createCell(rowNo++);//For Cadre
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==4){
-					 cell = row.createCell(rowNo++);//For Pran Status
+					 cell = row.createCell(rowNo++);//For Post Name
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==5){
-					 cell = row.createCell(rowNo++);//For Pran Remarks
+					 cell = row.createCell(rowNo++);//For Post Type
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==6){
+
+					 cell = row.createCell(rowNo++);//For Start Date
+					 if(obj[j]!=null)
+					 cell.setCellValue(obj[j].toString());
+					 /*
 					 cell = row.createCell(rowNo++);//For Employee Type
 					 if(obj[j]!=null)
 						 if(obj[j].toString()=="P") {
@@ -369,113 +223,92 @@ public class EmployeeStatisticsController {
 								cell.setCellValue("Temporary");
 							}
 						 
-				 }
+				 */}
+
 				 if(j==7){
-					 cell = row.createCell(rowNo++);//For Cadre
+					 cell = row.createCell(rowNo++);//For End Date
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				
 				 if(j==8){
-					 cell = row.createCell(rowNo++);//For Post Name
+					 cell = row.createCell(rowNo++);//For Date of joining
 					 if(obj[j]!=null)
 						 if(obj[j]!=null)
 							 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==9){
-					 cell = row.createCell(rowNo++);//For Post Type
+					 cell = row.createCell(rowNo++);//For Service End Date
 					 if(obj[j]!=null)
-						 if(obj[j].toString()=="P") {
-							 cell.setCellValue("Permanent");
-						}else if(obj[j].toString()=="B") {
-							 cell.setCellValue("Both");
-						}else {
-							cell.setCellValue("Temporary");
-						}
+						 if(obj[j]!=null)
+							 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==10){
-					 cell = row.createCell(rowNo++);//For Post Start Date
+					 cell = row.createCell(rowNo++);//For Scale Desc
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==11){
-					 cell = row.createCell(rowNo++);//For Post End Date
+					 cell = row.createCell(rowNo++);//For Basic Pay
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==12){
-					 cell = row.createCell(rowNo++);//For Date of Joining
+					 cell = row.createCell(rowNo++);//For 7Pc Basic
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
 				 if(j==13){
-					 cell = row.createCell(rowNo++);//For Date of Service Expiry
+					 cell = row.createCell(rowNo++);//For 7PC Level
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
+				///	 d.bank_branch_name,a.bank_acnt_no,a.pran_no
 				 if(j==14){
-					 cell = row.createCell(rowNo++);//For Scale Description
-					 if(obj[j]!=null)
-					 cell.setCellValue(obj[j].toString());
-				 }
-				 if(j==15){
-					 cell = row.createCell(rowNo++);//For Basic Salary
-					 if(obj[j]!=null)
-					 cell.setCellValue(obj[j].toString());
-				 }
-				 if(j==16){
-					 cell = row.createCell(rowNo++);//For Seven PC Basic
-					 if(obj[j]!=null)
-					 cell.setCellValue(Math.round(Double.parseDouble(obj[j].toString())));
-				 }
-				 if(j==17){
 					 cell = row.createCell(rowNo++);//For PF Series
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
-				 if(j==18){
+				 if(j==15){
 					 cell = row.createCell(rowNo++);//For GPF/DCPS Account No
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
-				 if(j==19){
+				 if(j==16){
 					 cell = row.createCell(rowNo++);//For GIS Group
 					 if(obj[j]!=null)
-					 cell.setCellValue(obj[j].toString());
+					 cell.setCellValue((obj[j].toString()));
 				 }
-				 if(j==20){
+				 if(j==17){
 					 cell = row.createCell(rowNo++);//For GIS Value
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
-				 if(j==21){
+				 if(j==18){
 					 cell = row.createCell(rowNo++);//For Physically Handicapped
-					 if(obj[j]!=null) {
-						 cell.setCellValue(obj[j].toString());
-					 }else {
-						 cell.setCellValue("-");
-					 }
+					 if(obj[j]!=null)
+					 cell.setCellValue(obj[j].toString());
 				 }
-				
-				 if(j==22){
+				 if(j==19){
 					 cell = row.createCell(rowNo++);//For Bank Name
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
-				 if(j==23){
-					 cell = row.createCell(rowNo++);//For Bank Branch Name
+				 if(j==20){
+					 cell = row.createCell(rowNo++);//For Branch Name
 					 if(obj[j]!=null)
 					 cell.setCellValue(obj[j].toString());
 				 }
-				 if(j==24){
-					 cell = row.createCell(rowNo++);//For IFSC Code
-					 if(obj[j]!=null)
+				 if(j==21){
+					 cell = row.createCell(rowNo++);//For Account no
+					 if(obj[j]!=null) {
 						 cell.setCellValue(obj[j].toString());
+					 }
 				 }
-				 if(j==25){
-					 cell = row.createCell(rowNo++);//For Account No
+				 if(j==22){
+					 cell = row.createCell(rowNo++);//For Pran No
 					 if(obj[j]!=null)
-						 cell.setCellValue(obj[j].toString());
+					 cell.setCellValue(obj[j].toString());
 				 }
 				
 				
@@ -524,53 +357,5 @@ public class EmployeeStatisticsController {
 }
 return map;
 }
-
-
-
-
-
-	private void employeeststicReportModel(String empname, String savaarthid, String dob, String empType, String cadre,
-			String postname, String postType, String poststartdate, String postenddate, String dateofjoining,
-			String dateofexipry, String scaledec, String basicpay, String sevenbasicpay, String pfserives,
-			String gpfdcps, String gisgroup, String gisvalue, String handicap, String bankname, String bankbarname,
-			String ifsccode, String accno,String pranno,String pranstatus,String pranremarks,String aadharno,String panno) {
-		EmployeeStatisticsModel employeeStatisticsModel = new EmployeeStatisticsModel();
-		employeeStatisticsModel.setEmpname(empname);
-		employeeStatisticsModel.setSavaarthid(savaarthid);
-		employeeStatisticsModel.setDob(dob);
-		employeeStatisticsModel.setEmpType(empType);
-		employeeStatisticsModel.setCadre(cadre);
-		employeeStatisticsModel.setPostname(postname);
-		employeeStatisticsModel.setPostType(postType);
-		employeeStatisticsModel.setPoststartdate(poststartdate);
-		employeeStatisticsModel.setPostenddate(postenddate);
-		employeeStatisticsModel.setDateofjoining(dateofjoining);
-		employeeStatisticsModel.setDateofexipry(dateofexipry);
-		employeeStatisticsModel.setScaledec(scaledec);
-		employeeStatisticsModel.setBasicpay(basicpay);
-		if(!sevenbasicpay.isEmpty() &&  sevenbasicpay!=null) {
-			employeeStatisticsModel.setSevenbasicpay(Math.round(Double.parseDouble(sevenbasicpay)));
-		}else {
-			employeeStatisticsModel.setSevenbasicpay(null);
-		}
-		employeeStatisticsModel.setPfserives(pfserives);
-		employeeStatisticsModel.setGpfdcps(gpfdcps);
-		employeeStatisticsModel.setGisgroup(gisgroup);
-		employeeStatisticsModel.setGisvalue(gisvalue);
-		employeeStatisticsModel.setHandicap(handicap);
-		employeeStatisticsModel.setBankname(bankname);
-		employeeStatisticsModel.setBankbarname(bankbarname);
-		employeeStatisticsModel.setIfsccode(ifsccode);
-		employeeStatisticsModel.setAccno(accno);
-		employeeStatisticsModel.setPrenno(pranno);
-		employeeStatisticsModel.setPrenstatus(pranstatus);
-		employeeStatisticsModel.setPrenremarks(pranremarks);
-		employeeStatisticsModel.setAadharno(aadharno);
-		employeeStatisticsModel.setPanno(panno);
-		lstempstcBillObj.add(employeeStatisticsModel);
-		
-	}
-
-	
 
 }
