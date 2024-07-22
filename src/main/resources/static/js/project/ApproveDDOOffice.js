6628f7515726ecdc0044640ef52b44d3be49d98f
Sudhir6re <sshahare2018@gmail.com>
2024-07-22 12:38:55 +0530

    Revert "error resolved"
    This reverts commit 0335ee9b83b14754003aa03ce2aaea2232ca67c5.

--- src/main/resources/static/js/project/ApproveDDOOffice.js
+++ src/main/resources/static/js/project/ApproveDDOOffice.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-	var contextPath = $("#appRootPath").val();
-
 function funDdo1() {
 
 		var schemeId = $('#cmbSchemeName').val();
@@ -8,27 +5,36 @@ function funDdo1() {
 		if(schemeId != "0"){
 			removeErrorClass($("#schemeName"));
 		}
+	
+
 		if (schemeId != '') {
 			$.ajax({
 				type : "GET",
-				url : contextPath+"/ddoast/getSchemeCodeAgainstName/" + schemeId,
+				url : "../ddoast/getSchemeCodeAgainstName/" + schemeId,
 				async : true,
 				contentType : 'application/json',
 				error : function(data) {
 					console.log(data);
+					/* alert("eror is" + data); */
 				},
-			 	beforeSend : function(){
-					$( "#loaderMainNew").show();
-					},
-				complete : function(data){
-					$( "#loaderMainNew").hide();
-				},
+
 				success : function(data) {
 					console.log(data);
 					$('#txtSchemeCode').val(0.);
 					$("#txtSchemeCode").val(data[0].schemeCode);
 					$("#cmbSubschemeName").val(data[0].schemeName);
 					$("#RadioPermenantTempBothP").val(data[0].schemeType);
+					/*var temp=data
+					 $("#txtSchemeCode").val('');
+					$.each(temp, function(index, value) {
+						$.each(value, function(index, value1) {
+							//console.log(index + "-" + value1);
+							if(index=="txtSchemeCode"){
+								console.log("txtSchemeCode");
+							
+							}
+						});
+					}); */
 				}
 			});
 		}
@@ -47,18 +53,26 @@ $(".officeName")
 					$
 							.ajax({
 								type : "GET",
-								url : contextPath+"/ddo/getAlreadySavedDataforDDO/"+ ddoCode,
+								url : "../ddo/getAlreadySavedDataforDDO/"+ ddoCode,
 								async : true,
 								contentType : 'application/json',
 								error : function(
 										data) {
 									console.log(data);
+									
 									$(".loaderMainNew").hide();
+									// alert(data);
 								},
 								success : function(
 										data) {
+									
+									
 									var temp=data;
 									$(".loaderMainNew").hide();
+									//$(".txtSchemeCode").val(data[3]);
+									
+									///office_name,state,district,taluka,town,village,address1,office_pin,office_city_class,dice_code,grant_applicable,tel_no1,fax,tribal_area_flag,
+									///email,hilly_area_flag,naxalite_area_flag
 									$.each(temp,function(index,value) {
 												$('#txtNameOfOffice').val(value[0]);
 												$('#stateId').val(value[1]);
@@ -91,6 +105,8 @@ $(".officeName")
 													 $('#RadioButtonNaxaliteAreaYes').prop("checked",true);
 												 else
 													 $('#RadioButtonNaxaliteAreaNo').prop("checked",true);
+												 
+												
 												
 											});
 									
@@ -113,21 +129,16 @@ $('#approve')
 				$
 						.ajax({
 							type : "GET",
-							url : contextPath+"/ddo/updateApproveRejectStatus/"
+							url : "../updateApproveRejectStatus/"
 									+ ddoCode +"/"+flag,
 							async : true,
 							contentType : 'application/json',
 							error : function(data) {
 								console.log(data);
 							},
-						 	beforeSend : function(){
-								$( "#loaderMainNew").show();
-								},
-							complete : function(data){
-								$( "#loaderMainNew").hide();
-							},
 							success : function(data) {
 								console.log(data);
+								// alert(data);
 									swal(
 											"Approved Successfully",
 											{
@@ -148,28 +159,24 @@ $('#btnRjct')
 				$
 				.ajax({
 					type : "GET",
-					url : contextPath+"/ddo/updateApproveRejectStatus/"
+					url : "../updateApproveRejectStatus/"
 						+ ddoCode +"/"+flag,
 						async : true,
 						contentType : 'application/json',
 						error : function(data) {
 							console.log(data);
 						},
-					 	beforeSend : function(){
-							$( "#loaderMainNew").show();
-							},
-						complete : function(data){
-							$( "#loaderMainNew").hide();
-						},
 						success : function(data) {
 							console.log(data);
+							// alert(data);
 							swal(
 									"Rejected Successfully",
 									{
 										icon : "success",
 									});
+							
+							
 						}
 				});
 			}
 		});
-});
