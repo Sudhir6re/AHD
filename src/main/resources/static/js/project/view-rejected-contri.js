c2841c0974e1baf72286c5ed007dc592440617c2
pratikshinde788 <>
2024-08-21 09:51:10 +0530

    validations

--- src/main/resources/static/js/project/view-rejected-contri.js
+++ src/main/resources/static/js/project/view-rejected-contri.js
@@ -1,39 +1,14 @@
 jQuery(document).ready(function($) {
 	var varMessage = $("#message").val();
-	if(varMessage != "" && varMessage != undefined) {
-		swal(''+varMessage, {
-  	      icon: "success",
-  	  });
+	if (varMessage != "" && varMessage != undefined) {
+		swal('' + varMessage, {
+			icon : "success",
+		});
 	}
-	});
-
-
-$("#delayedMonthAndYearCombos").hide()
-$("#DAArrearsDatesDivDisplay").hide()
-$("#PayArrearsDatesDivDisplay").hide()
-function addMonthYearComboForDelayed() {
-	var typeOfPaymentMaster = $("#cmbTypeOfPaymentMaster").val();
-	if (typeOfPaymentMaster == '700047') {
-		$("#delayedMonthAndYearCombos").fadeIn(100);
-	} else {
-		$("#delayedMonthAndYearCombos").hide()
-	}
-
-	if (typeOfPaymentMaster == '700048') {
-		$("#DAArrearsDatesDivDisplay").fadeIn(100);
-	} else {
-		$("#DAArrearsDatesDivDisplay").hide();
-	}
-
-	if (typeOfPaymentMaster == '700049') {
-		$("#PayArrearsDatesDivDisplay").fadeIn(100);
-	} else {
-		$("#PayArrearsDatesDivDisplay").hide();
-	}
-}
+});
 
 $(document).ready(function() {
-	$("form[name='onlineEntryForm']").validate({
+	$("form[name='viewRejectedContri']").validate({
 		ignore : "",
 		rules : {
 			cmbTreasuryCode : {
@@ -87,5 +62,5 @@ $(document).ready(function() {
 			form.submit();
 		}
 	});
-	
+
 });
