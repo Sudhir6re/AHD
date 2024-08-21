b471a27e2d76853e22ea9513b88d1462986f58e3
unknown <tejashree.egov@gmail.com>
2024-08-21 10:04:48 +0530

    committed for online contribution

--- src/main/java/com/mahait/gov/in/controller/OnlineContriEntryController.java
+++ src/main/java/com/mahait/gov/in/controller/OnlineContriEntryController.java
@@ -1,20 +1,45 @@
 package com.mahait.gov.in.controller;
 import java.util.Locale;
 import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mahait.gov.in.entity.OrgUserMst;
+import com.mahait.gov.in.model.RegularReportModel;
+import com.mahait.gov.in.service.CommonHomeMethodsService;
+import com.mahait.gov.in.service.OnlineContributionService;
+import com.mahait.gov.in.service.RegularReportService;
 @RequestMapping("/ddoast")
 @Controller
 public class OnlineContriEntryController   extends BaseController{
+	
+	@Autowired
+	CommonHomeMethodsService commonHomeMethodsService;
+	
+	@Autowired
+	RegularReportService regularReportService;
+	
+	@Autowired
+	OnlineContributionService onlineContributionService;
+	
 	@GetMapping("/onlineContriEntry")
-	public String onlineContriEntry( Model model, Locale locale,
-			HttpSession session) {
+	public String onlineContriEntry( @ModelAttribute("regularReportModel") RegularReportModel regularReportModel,
+			Model model, Locale locale, HttpSession session) {
 		OrgUserMst messages = (OrgUserMst) session.getAttribute("MY_SESSION_MESSAGES");
 		addMenuAndSubMenu(model,messages);
+		model.addAttribute("regularReportModel", regularReportModel);
+		model.addAttribute("lstMonths", commonHomeMethodsService.lstGetAllMonths());
+		model.addAttribute("lstYears", commonHomeMethodsService.lstGetAllYears());
+		model.addAttribute("lstBillDesc", regularReportService.lstBillDesc(messages.getDdoCode()));
+		model.addAttribute("ddoNameLst", regularReportService.getDDOName(messages.getDdoCode()));
+		model.addAttribute("paymentTypeLst", onlineContributionService.getPaymentTypeLst());
+		
+		
 		return "/views/online-contri-entry";
 	}
 	@GetMapping("/viewRejectedContri")
--- src/main/resources/static/js/project/online-contri-entry.js
+++ src/main/resources/static/js/project/online-contri-entry.js
@@ -1,3 +1,12 @@
+var contextPath =
+$(document).ready(function() {
+	 contextPath = $("#appRootPath").val();
+	  if ($('#cmbSchemeName').length) {
+	        $('#cmbSchemeName').select2();
+	    }
+});
+
+
 $("#delayedMonthAndYearCombos").hide()
 $("#DAArrearsDatesDivDisplay").hide()
 $("#PayArrearsDatesDivDisplay").hide()
@@ -20,4 +29,39 @@ function addMonthYearComboForDelayed() {
 	} else {
 		$("#PayArrearsDatesDivDisplay").hide();
 	}
+}
+
+
+
+function funDdo1() {
+
+	var schemeId = $('#billGroup').val();
+	
+	if(schemeId != "0"){
+		removeErrorClass($("#schemeName"));
+	}
+
+	if (schemeId != '') {
+		$.ajax({
+			type : "GET",
+			url : contextPath+"/ddoast/getSchemeCodeAgainstName/" + schemeId,
+			async : true,
+			contentType : 'application/json',
+			error : function(data) {
+				console.log(data);
+			},
+			beforeSend : function(){
+				$( "#loaderMainNew").show();
+				},
+			complete : function(data){
+				$( "#loaderMainNew").hide();
+			},	
+			success : function(data) {
+				console.log(data);
+				$("#txtSchemeName").val(data[0].schemeName);
+				$("#txtSubSchemeName").val("-");
+			}
+		});
+	}
+
 }
