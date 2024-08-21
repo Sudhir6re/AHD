7071fae9a5fdebfc4872d4f30b316e7b8fc98f86
Komal <komal.egov@gmail.com>
2024-08-21 10:48:22 +0530

    DCPS-GPF

---
+++ src/main/java/com/mahait/gov/in/service/ChangeDcpsGpfServiceImpl.java
@@ -0,0 +1,50 @@
+package com.mahait.gov.in.service;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mahait.gov.in.entity.DdoOffice;
+import com.mahait.gov.in.entity.MstEmployeeEntity;
+import com.mahait.gov.in.entity.OrgUserMst;
+import com.mahait.gov.in.model.MstEmployeeModel;
+import com.mahait.gov.in.repository.ChangeDcpsGpfRepo;
+
+@Service
+@Transactional
+public class ChangeDcpsGpfServiceImpl implements ChangeDcpsGpfService {
+	
+	@Autowired
+	ChangeDcpsGpfRepo changeDcpsGpfRepo;
+
+	@Override
+	public List<Object[]> getDOJ(String sevaarthId) {
+		// TODO Auto-generated method stub
+		return changeDcpsGpfRepo.getDOJ(sevaarthId);
+	}
+
+	@Override
+	public Long savechangeDcpsGpf(@Valid MstEmployeeModel mstEmployeeModel, OrgUserMst messages) {
+		MstEmployeeEntity lstprop = changeDcpsGpfRepo.getemp(messages.getDdoCode());
+
+		
+	
+        lstprop.setDoj(mstEmployeeModel.getDoj());
+        if(mstEmployeeModel.getDcpsgpfflag()=="D") {
+        	lstprop.setDcpsgpfflag("Y");
+        } else if(mstEmployeeModel.getDcpsgpfflag()=="G") {
+        	lstprop.setDcpsgpfflag("N");
+        }
+        
+       
+		
+        changeDcpsGpfRepo.savechangeDcpsGpf(lstprop);
+        return 10l;
+	}
+
+}
