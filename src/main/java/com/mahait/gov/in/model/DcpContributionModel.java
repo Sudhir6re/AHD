fe16224636ccafc473fea35f2789300b2f45d7ee
Sudhir6re <sshahare2018@gmail.com>
2024-08-22 16:11:34 +0530

    dcps entity

---
+++ src/main/java/com/mahait/gov/in/model/DcpContributionModel.java
@@ -0,0 +1,77 @@
+package com.mahait.gov.in.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import lombok.Data;
+
+
+@Data
+public class DcpContributionModel {
+	
+    private Long dcpContributionId;
+
+    private Long dcpEmpId;
+
+    private Integer treasuryCode;
+
+    private String ddoCode;
+
+    private Long billGroupId;
+
+    private String schemeCode;
+
+    private String payCommission;
+
+    private String typeOfPayment;
+
+    private Long finYearId;
+
+    private Long monthId;
+
+    private Float basicPay;
+
+    private Float da;
+
+    private Float contribution;
+
+    private Integer regStatus;
+
+    private Long langId;
+
+    private Long locId;
+
+    private Long dbId;
+
+    private Long createdPostId;
+
+    private Long createdUserId;
+
+    private Timestamp createdDate;
+
+    private Long updatedPostId;
+
+    private Long updatedUserId;
+
+    private Timestamp updatedDate;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Long rltContriVoucherId;
+
+    private Long delayedFinYearId;
+
+    private Long delayedMonthId;
+
+    private String employerContriFlag;
+
+    private String status;
+
+    private Date voucherDate;
+
+    private String voucherNo;
+
+
+}
