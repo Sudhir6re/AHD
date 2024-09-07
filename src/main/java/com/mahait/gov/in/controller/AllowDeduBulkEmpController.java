9de3fc0025392915299430635f3cc368bcf7bcd1
Sudhir6re <sshahare2018@gmail.com>
2024-09-07 11:32:28 +0530

    error fix

--- src/main/java/com/mahait/gov/in/controller/AllowDeduBulkEmpController.java
+++ src/main/java/com/mahait/gov/in/controller/AllowDeduBulkEmpController.java
@@ -30,7 +30,6 @@ import com.mahait.gov.in.service.AllowanceDeductionWiseMstService;
 import com.mahait.gov.in.service.CommonHomeMethodsService;
 import com.mahait.gov.in.service.CreateAdminOfficeService;
 import com.mahait.gov.in.service.DeptEligibilityForAllowAndDeductService;
-import com.mahait.gov.in.service.MpgSchemeBillGroupService;
 import com.mahait.gov.in.service.MstEmployeeService;
 
 @Controller
@@ -48,10 +47,10 @@ public class AllowDeduBulkEmpController {
 	
 	@Autowired
 	MstEmployeeService mstEmployeeService;
-	
+/*	
 	@Autowired
 	MpgSchemeBillGroupService mpgSchemeBillGroupService;
-	
+	*/
 	
 	@Autowired
 	AllowanceDeductionWiseMstService allowanceDeductionWiseMstService;
@@ -80,8 +79,8 @@ public class AllowDeduBulkEmpController {
 		model.addAttribute("lstdeptEligibilityForAllowAndDeduct", deptEligibilityForAllowAndDeductService.findDeptNonGovDeductList());
 		model.addAttribute("context", request.getContextPath());
 
-		model.addAttribute("lstSchemeBillGroup", mpgSchemeBillGroupService
-				.findAllMpgSchemeBillGroupByDDOCode(messages.getUserName()));
+/*		model.addAttribute("lstSchemeBillGroup", mpgSchemeBillGroupService
+				.findAllMpgSchemeBillGroupByDDOCode(messages.getUserName()));*/
 		
 		
 		
