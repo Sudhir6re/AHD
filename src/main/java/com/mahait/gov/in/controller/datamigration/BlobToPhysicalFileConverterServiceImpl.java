ea9d6dd59efad49857ac44bb0f8ef59a7fd0a62b
Sudhir6re <sshahare2018@gmail.com>
2024-09-19 17:21:09 +0530

    paybill

--- src/main/java/com/mahait/gov/in/controller/datamigration/BlobToPhysicalFileConverterServiceImpl.java
+++ src/main/java/com/mahait/gov/in/controller/datamigration/BlobToPhysicalFileConverterServiceImpl.java
@@ -16,11 +16,13 @@ import java.util.List;
 import javax.imageio.ImageIO;
 import javax.transaction.Transactional;
 
+import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.mahait.gov.in.common.StringHelperUtils;
+import com.mahait.gov.in.entity.GROrderDocumentEntity;
 import com.mahait.gov.in.entity.MstEmployeeEntity;
 
 
@@ -47,14 +49,13 @@ public class BlobToPhysicalFileConverterServiceImpl implements BlobToPhysicalFil
 			List<byte[]> lst = new ArrayList<>();
 			String fileExtension = getFileExtension(byteData, tika);
 			String fullFileName = fileName + "." + fileExtension;
-
 			String DeptNm = "";
 
 			if (typeOp == 1) {
 				String filePath = uploadPhoto(byteData, DeptNm, id);
 				if (filePath != null) {
 
-				//	MstEmployeeEntity mstEmployeeEntity = blobToPhysicalFileConverterRepo.findEmpByEmpId(id);
+					MstEmployeeEntity mstEmployeeEntity = blobToPhysicalFileConverterRepo.findEmpByEmpId(id);
 					//mstEmployeeEntity.setPhotoAttachmentId(filePath);
 					//mstEmployeeEntity.setEmployeeId(id.longValue());
 					//blobToPhysicalFileConverterRepo.updatePhotoPath(mstEmployeeEntity);
@@ -71,10 +72,10 @@ public class BlobToPhysicalFileConverterServiceImpl implements BlobToPhysicalFil
 				
 				String filePath = saveAttachment(byteData, id, fileExtension);
 				if (filePath != null) {
-					/*BigInteger grDocumentId = StringHelperUtils.isNullBigInteger(row[3]);
+					BigInteger grDocumentId = StringHelperUtils.isNullBigInteger(row[3]);
 					GROrderDocumentEntity gROrderDocumentEntity =blobToPhysicalFileConverterRepo.findDocumentId(grDocumentId);
 					gROrderDocumentEntity.setFilePath(filePath);
-					blobToPhysicalFileConverterRepo.updateGrPath(gROrderDocumentEntity);*/
+					blobToPhysicalFileConverterRepo.updateGrPath(gROrderDocumentEntity);
 				}
 
 			}
